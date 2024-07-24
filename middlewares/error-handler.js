38eb6830f2770b52392d4d7e7e739e950c791d95
novanish <anishchhetri196@gmail.com>
2024-07-24 20:41:57 +0545

    Enhance error handler to handle MongoDB and Mongoose specific errors

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -6,6 +6,25 @@ function errorHandler(err, req, res, next) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
 
+  if (err.code === 11000) {
+    const msg = `Duplicate Field Value Enter: ${Object.keys(err.keyValue)}`;
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg });
+  }
+
+  if (err.name === "CastError") {
+    const msg = `Resource not found. Invalid: ${err.path}`;
+    return res.status(StatusCodes.NOT_FOUND).json({ msg });
+  }
+
+  if (err.name === "ValidationError") {
+    const msg = Object.values(err.errors)
+      .map((e) => e.message)
+      .join(", ");
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg });
+  }
+
+  console.error(err);
   res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .json({ msg: "Something went wrong, please try again" });
