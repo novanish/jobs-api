4e9afddb44ea37bac70541c63a6b04a16187c7d8
novanish <anishchhetri196@gmail.com>
2024-07-25 21:22:28 +0545

    fix: configure rate limiter to trust proxy headers

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const PORT = process.env.PORT || 3000;
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100,
+  trustProxy: true,
 });
 const swaggerDocument = YAML.load("./swagger.yaml");
 
