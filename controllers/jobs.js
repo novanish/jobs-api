a652257844e9d43e5711a3a43a9efb541ff64017
novanish <anishchhetri196@gmail.com>
2024-07-25 19:39:46 +0545

    feat: implement job management endpoints

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,54 @@
+const { StatusCodes } = require("http-status-codes");
+const NotFoundError = require("../errors/NotFoundError");
+
+const Job = require("../models/Job");
+
 const getAllJobs = async (req, res) => {
-  res.status(200).send("All Jobs");
+  const jobs = await Job.find({ createdBy: req.payload.userId }).sort(
+    "-updatedAt -createdAt"
+  );
+
+  res.json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
-  res.status(200).send("Single Job");
+  const jobId = req.params.id;
+  const userId = req.payload.userId;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) throw new NotFoundError(`No job found with id: ${jobId}`);
+
+  res.json({ job });
 };
 
 const createJob = async (req, res) => {
-  res.status(201).send("Job Created");
+  const job = { ...req.body, createdBy: req.payload.userId };
+  const newJob = await Job.create(job);
+  res.status(StatusCodes.CREATED).json({ job: newJob });
 };
 
 const updateJob = async (req, res) => {
-  res.status(200).send("Job Updated");
+  const jobId = req.params.id;
+  const userId = req.payload.userId;
+
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) throw new NotFoundError(`No job found with id: ${jobId}`);
+
+  res.json({ job });
 };
 
 const deleteJob = async (req, res) => {
-  res.status(200).send("Job Deleted");
+  const jobId = req.params.id;
+  const userId = req.payload.userId;
+
+  const job = await Job.findByIdAndDelete({ _id: jobId, createdBy: userId });
+  if (!job) throw new NotFoundError(`No job found with id: ${jobId}`);
+
+  res.status(200).json({ job });
 };
 
 module.exports = {
